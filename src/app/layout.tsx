ba9e3b1219f702f0c041982ba331f4a6012894ca
Irvan Maulana Ahmad <vanir1812@gmail.com>
2026-05-10 03:39:23 +0700

    feat: configure dark themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RscBoundaryProvider } from "@rsc-boundary/next";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,17 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <RscBoundaryProvider>{children}</RscBoundaryProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <RscBoundaryProvider>{children}</RscBoundaryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
