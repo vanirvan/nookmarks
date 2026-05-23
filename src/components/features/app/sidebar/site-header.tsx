8fb54b32f4277a3b2a6cca69309d7d9e66c956d9
Irvan Maulana Ahmad <vanir1812@gmail.com>
2026-05-23 20:42:08 +0700

    changes: remove unused search form in the site-header

--- src/components/features/app/sidebar/site-header.tsx
+++ src/components/features/app/sidebar/site-header.tsx
@@ -1,5 +1,4 @@
 import { NavBreadcrumb } from "@/components/features/app/sidebar/nav-breadcrumb";
-import { SearchForm } from "@/components/features/app/sidebar/search-form";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
@@ -7,14 +6,15 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 flex w-full items-center border-b bg-background">
-      <div className="flex h-(--header-height) w-full items-center gap-4 px-4">
-        <SidebarTrigger />
-        <Separator
-          orientation="vertical"
-          className="data-vertical:h-4 data-vertical:self-auto"
-        />
-        <NavBreadcrumb />
-        <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+      <div className="flex h-(--header-height) w-full items-center justify-between gap-4 px-4">
+        <div className="flex items-center gap-4">
+          <SidebarTrigger />
+          <Separator
+            orientation="vertical"
+            className="data-vertical:h-4 data-vertical:self-auto"
+          />
+          <NavBreadcrumb />
+        </div>
         <AnimatedThemeToggler />
       </div>
     </header>
