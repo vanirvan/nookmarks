8a6bd7ae7b7fe81fc0a1cb92c6154db25f135ca3
Irvan Maulana Ahmad <vanir1812@gmail.com>
2026-09-18 14:04:28 +0700

    changes(db): add package 'pg' and modify db cofig to support local development with local database while not modifying productions db

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,20 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool as NeonPool } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
+import { Pool as PgPool } from "pg";
 import * as relations from "./relations";
 import * as schema from "./schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL ?? "" });
-export const db = drizzle({
-  client: pool,
-  schema: { ...schema, ...relations },
-});
+const schemaConfig = { ...schema, ...relations };
+const url = process.env.DATABASE_URL ?? "";
+
+export const db =
+  process.env.NODE_ENV === "production"
+    ? drizzleNeon({
+        client: new NeonPool({ connectionString: url }),
+        schema: schemaConfig,
+      })
+    : drizzlePg({
+        client: new PgPool({ connectionString: url }),
+        schema: schemaConfig,
+      });
